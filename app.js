c3d1091abac1c73303aacac86b75529380b056ef
Adriano MotoGS+ <adrianordp@alu.ufc.br>
2020-02-12 13:21:40 -0300

    check file change

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const express = require('express');
+//const express = require('express');
 
-const app = express();
+//const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hey whas up from express update');
-});
+//app.get('/', (req, res) => {
+//    res.send('hey whas up from express update');
+//});
 
-console.log('Listening to 192.168.100.156:3000');
+//console.log('Listening to 192.168.100.156:3000');
 
-app.listen(3000);
+//app.listen(3000);
+
+const fs = require('fs');
+
+const filename = 'target.txt';
+console.log('start tracking ' filename);
+fs.watch(filename, () => console.log('File changed!'));
