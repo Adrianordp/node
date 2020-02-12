b925f4abf86fa734d5f43de925f44babe5125056
Adriano MotoGS+ <adrianordp@alu.ufc.br>
2020-02-12 13:04:46 -0300

    Getting a updated message

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hey whas up from express');
+    res.send('hey whas up from express update');
 });
+
+console.log('Listening to 192.168.100.156:3000');
 
 app.listen(3000);
