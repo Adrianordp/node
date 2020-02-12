af103b7b3d7bed056176a6b013780da31a7c6365
Adriano Debian <adrianordp@alu.ufc.br>
2020-02-12 14:17:37 -0300

    Assync node js

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
-//const express = require('express');
-
-//const app = express();
-
-//app.get('/', (req, res) => {
-//    res.send('hey whas up from express update');
-//});
-
-//console.log('Listening to 192.168.100.156:3000');
-
-//app.listen(3000);
-
 const fs = require('fs');
+const fileName = 'target.txt';
 
-const filename = 'target.txt';
-console.log('start tracking ' filename);
-fs.watch(filename, () => console.log('File changed!'));
+const errHandler = (err) => console.log(err);
+
+const dataHandler = (data) => console.log(data.toString());
+
+fs.readFile(fileName, (err, data) => {
+    if(err) errHandler(err);
+    dataHandler(data);
+});
+
+console.log('Node js async programming')
